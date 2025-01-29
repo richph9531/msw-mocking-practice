c8ff405c73a0e95d6cadca1c27906baf166e436e
Richard Philips <richard.philips@johnlewis.co.uk>
2025-01-29 15:54:49 +0200

    initial commit

---
+++ pages/countries.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from '@/styles/Countries.module.css'
+import Link from 'next/link'
+
+export default function Posts(props) {
+  const sortedCountries = props.countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
+
+  return (
+    <div>
+      {sortedCountries.map((country) => (
+        <div key={country.name.common}>
+          <Link href={`/country/${country.name.common}`} className={styles.countryInfo}>{country.name.common}</Link>
+        </div>
+      ))}
+      </div>    
+  )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const retrievedCountries = await response.json();
+  return {
+    props: {
+      countries: retrievedCountries
+    }
+  };
+} 
